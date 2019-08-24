59e3e956b7828064a52491a170bff8a307b07b82
pacastega <p.cas.gar@gmail.com>
2019-08-24 19:18:20 +0200

    Mejoras en el sistema de movimiento y colisiones

--- Pantalla1.py
+++ Pantalla1.py
@@ -8,37 +8,22 @@ from Variables import *
 
 class Pantalla1(Scene):
 
-    def __init__(self,map,image):
+    def __init__(self, map, image):
         Scene.__init__(self, map, image)
-        self.pj = Personaje(map, 200, 200)
+        self.pj = Personaje(200, 200)
         self.ingame_elemets = pygame.sprite.Group()
         self.ingame_elemets.add(self.pj)
+        self.mapa = map
 
-        self.plataformas = [
+        self.obs = [
             Plataforma(150, 500),
             Plataforma(550, 500),
             Plataforma(0, 700, 1000, 5)
         ]
 
-    def on_update(self, time,keys):
+    def on_update(self, time, keys):
         self.camera.update(self.pj)
-
-        ## nuevo
-        tocado_suelo = False
-        for plat in self.plataformas:
-            if self.pj.suelo(plat):
-                tocado_suelo = True
-                self.pj.saltando = False
-                print("Tocando suelo!")
-                
-        if not tocado_suelo:
-            self.pj.saltando = True
-            print("Flotando")
-        ##
-
-
-
-        self.ingame_elemets.update(time/1000, keys)
+        self.ingame_elemets.update(time/1000, keys, self.mapa, self.obs)
 
         if keys[K_RETURN]:
             return Pantalla2(map2,"imagenes/test.png")
@@ -48,13 +33,11 @@ class Pantalla1(Scene):
     def on_draw(self, screen):
         screen.blit(self.background, self.camera.apply(self.background.get_rect()))
         for i in self.ingame_elemets:
-            screen.blit(i.image, self.camera.apply(i.rect))
+            screen.blit(i.image, self.camera.apply(i.rect).move(0, -44))
 
-        ## nuevo
-        for plat in self.plataformas:
-            screen.blit(plat.image, plat.rect)
+        for o in self.obs:
+            screen.blit(o.image, o.rect)
         pygame.draw.rect(screen, (0,0,127), self.pj.rect)
-        ##
 
 
 class Pantalla2(Scene):
--- Personaje.py
+++ Personaje.py
@@ -5,44 +5,36 @@ from Variables import *
 
 class Personaje(sprite.Sprite):
 
-    def __init__(self, mapa, x=0, y=0):
+    def __init__(self, x=0, y=0):
         #Init de Sprite
         sprite.Sprite.__init__(self)
-        ''' Cargamos la hoja completa de sprites del personaje.
-            Se realiza convert_alpha() para que tenga en cuenta transparencias (capa alpha)
-        '''
-        
+
+        # Variables para nuestro control del sprite:
+        self.frames = 4               # Número máximo de imágenes
+        self.current_frame = 0        # Imagen actual
+        self.frame_width = 64         # Anchura de la imagen
+        self.frame_height = 64        # Altura de la imagen
+        self.frame_counter = FPSPRITE # Nº de frames por imagen
+
+        # Cargamos la hoja completa de sprites del personaje.
+        # Se realiza convert_alpha() para que tenga en cuenta transparencias (capa alpha)
         self.spriteSheet = pygame.image.load("imagenes/pok.png").convert_alpha()
         # "image" se corresponde con la imagen actual a mostrar.
-        #La hacemos más pequeña para que quede mejor
-        self.image = self.spriteSheet.subsurface((0,0,64,64))
-        #Necesario para mostrar la imagen
+        self.image = self.spriteSheet.subsurface(0,0,self.frame_width,self.frame_height)
+        # Collision box:
         self.rect = self.image.get_rect()
-        #Donde se situa la imagen.
-        if (x==0 and y==0):
-            self.rect.center = (mapa[0]//2, mapa[1]//2)
-        else:
-            self.rect.center = (x, y)
-
-        self.mapa = Rect(0,0,mapa[0],mapa[1])
-
-        ''' Variables para nuestro control del sprite
-        '''
-        self.frames = 4             #Número máximo de imágenes
-        self.current_frame = 0      #Frame actual
-        self.nframes = 4 # number of sprites/direction
-        self.frame_width = 64     #Anchura de la imagen
-        self.frame_height = 64     #Altura dela imagen
-        self.frame_counter = FPSPRITE     #number of frames per sprite
+        self.rect = pygame.Rect(x, y, self.rect.width, 20)
+        
+        # Control del movimiento:
         self.speedx = 5
         self.speedy = 0
-        self.saltando = True
+        self.saltando = False
 
 
-    def update(self, dt, keys):
+    def update(self, dt, keys, mapa, obs):
         # animaciones:
         if self.frame_counter == 0:
-            self.current_frame = (self.current_frame + 1) % self.nframes # siguiente sprite
+            self.current_frame = (self.current_frame + 1) % self.frames # siguiente sprite
             self.frame_counter = FPSPRITE
         else:
             self.frame_counter -= 1
@@ -52,52 +44,51 @@ class Personaje(sprite.Sprite):
             self.image = self.spriteSheet.subsurface((0,0, self.frame_width, self.frame_height))
 
         # movimiento y animación según la dirección de movimiento:
-        if keys[K_DOWN] :
+        if keys[K_DOWN]:
             self.image = self.spriteSheet.subsurface((self.current_frame * self.frame_width,
-                                                          0,
-                                                          self.frame_width, self.frame_height))
-            self.move(0, self.speedy)
-        if keys[K_LEFT] :
+                                                      0,
+                                                      self.frame_width, self.frame_height))
+            self.move((0, self.speedy), mapa, obs)
+        if keys[K_LEFT]:
             self.image = self.spriteSheet.subsurface((self.current_frame * self.frame_width,
-                                                          self.frame_height,
-                                                          self.frame_width, self.frame_height))
-            self.move(-self.speedx)
-        if keys[K_RIGHT] :
+                                                      self.frame_height,
+                                                      self.frame_width, self.frame_height))
+            self.move((-self.speedx, 0), mapa, obs)
+        if keys[K_RIGHT]:
             self.image = self.spriteSheet.subsurface((self.current_frame * self.frame_width,
-                                                          2*self.frame_height,
-                                                          self.frame_width, self.frame_height))
-            self.move(self.speedx)
-        if keys[K_UP] :
+                                                      2*self.frame_height,
+                                                      self.frame_width, self.frame_height))
+            self.move((self.speedx, 0), mapa, obs)
+        if keys[K_UP]:
             self.image = self.spriteSheet.subsurface((self.current_frame * self.frame_width,
-                                                          3*self.frame_height,
-                                                          self.frame_width, self.frame_height))
-            self.move(0,-self.speedy)
-        
+                                                      3*self.frame_height,
+                                                      self.frame_width, self.frame_height))
+            self.move((0,-self.speedy), mapa, obs)
+
         # salto:
         if keys[K_SPACE] and not self.saltando:
             self.saltando = True
             self.speedy = -10
 
         if self.saltando:
-            self.move(0, self.speedy)
-            self.speedy += 1 #gravedad
+            self.move((0, self.speedy), mapa, obs)
+            self.speedy += 1 # gravedad
         else:
             self.speedy = 0
 
-        ## nuevo
         if keys[K_r]:
             self.rect.center = (200, 200)
+
+    def move(self, offset, mapa, obs):
+        self.rect = self.rect.move(offset) # avanzamos
+        #ponemos velocidad baja:
+        offset = list(offset)
+        if offset[0] != 0: offset[0] = -abs(offset[0])/offset[0]
+        if offset[1] != 0: offset[1] = -abs(offset[1])/offset[1]
             
-        ##
-
-
-    def move(self, x=0, y=0):
-        if self.rect.centerx+x>=self.mapa.width or self.rect.centerx+x <= 0:
-            return
-        if self.rect.centery+y>=self.mapa.height or self.rect.centery+y <= 0:
-            return
-        self.rect.center = (self.rect.centerx+x, self.rect.centery+y)
-
-    def suelo(self, otro_rect):
-        return self.rect.colliderect(otro_rect)
-        
+        while not self.pos_valida(mapa, obs): # mientras la posición no sea válida
+            self.rect = self.rect.move(offset) # retrocedemos poco a poco
+            
+    def pos_valida(self, mapa, obs):
+        return self.rect.collidelist(obs)==-1 and self.rect.bottom-self.frame_height>=0 and \
+            self.rect.left>=0 and self.rect.right<=mapa[0] and self.rect.bottom<=mapa[1]
