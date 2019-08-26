ec44f8118647ff335aca6ab68bc478a4beff7c94
jorgelzn <jorgelizgo@gmail.com>
2019-08-26 20:15:58 +0200

    sonidos

--- sonidos.py
+++ sonidos.py
@@ -4,4 +4,4 @@ class Sonido:
 
     def __init__(self):
         self.soundtrack1 = pygame.mixer.music.load("sonidos/ost/Moki_Town.mp3")
-        Self.pointerSound = pygame.mixer.Sound("sonidos/effects/Back.mp3")
+        self.pointerSound = pygame.mixer.Sound("sonidos/effects/test.wav")
