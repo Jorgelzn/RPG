204c59459cf65d577b41387036fefb80ad8df11a
jorgelzn <jorgelizgo@gmail.com>
2019-08-27 17:04:58 +0200

    imagenes varias

--- Escenarios.py
+++ Escenarios.py
@@ -2,6 +2,6 @@ import pygame
 
 class Obstaculo:
     def __init__(self, imagen, x=0, y=0, w=200, h=200):
-        self.image = pygame.image.load(imagen)
-        self.image = pygame.transform.scale(self.image, (w, h))
+        self.spriteSheet = pygame.image.load(imagen).convert_alpha()
+        self.image = self.spriteSheet.subsurface(0,0,w,h)
         self.rect = pygame.Rect(x, y, w, h)
