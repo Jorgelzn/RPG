5f774cf49d23d89f21ca52a0b06e075ff0dc4e04
jorgelzn <jorgelizgo@gmail.com>
2019-09-05 12:47:50 +0200

    check point

--- Pantalla1.py
+++ Pantalla1.py
@@ -19,7 +19,7 @@ class Pantalla1(Scene):
         self.mapa = map
         self.sonido = Sonido()
         self.npcs = [
-            Npc("imagenes/personajes/paperi_sheet.png",["hola","soy paperi","buenos dias"], 600, 100, 68, 189),
+            Npc("imagenes/personajes/paperi_sheet.png",["hola","soy paperi","buenos dias","ven a nuestra tienda"], 600, 100, 68, 189),
             Npc("imagenes/personajes/Peto_sheet.png",["paperi siempre esta igual","deberiamos llamarnos peto y paperi","mamahuevo"],500, 600, 114, 145),
             Npc("imagenes/personajes/Tapa_sheet.png",["me llamo Tapa :D","vente luego a jugar"],1200, 600, 68, 77),
             Npc("imagenes/personajes/Kea_sheet.png",["me preocupan paperi y peto","no paran de pelear"],1200, 200, 84, 132)
